f8a3c9df4e9b232a7026db8b463d01ac0b6bfa8a
DAKSHATA SURESH BORSE <128856294+dakshata03@users.noreply.github.com>
2023-08-24 07:19:09 +0530

    Update sum.c

--- tasks/c/easy/sum.c
+++ tasks/c/easy/sum.c
@@ -3,9 +3,24 @@
 #include <stdio.h>
 
 // TODO: Implement the sum function
+int sum(int a, int b) {
+    if (b == 0) {
+        return a;
+    } else {
+        int carry = (a & b) << 1;
+        int add = a ^ b;
+        return sum(add, carry);
+    }
+}
+ 
 
 int main() {
     // TODO: Implement the main function
+    int x,y;
+    printf("enter num:");
+    scanf("%d %d",&x,&y);
+    int r=sum(x,y);
+    printf(" \n sum is : %d",r);
 
     return 0;
 }
